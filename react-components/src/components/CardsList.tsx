ac12e30d440d67927230626bae52b47f1d0897bc
Igor <igoralyanoy@gmail.com>
2022-03-26 17:39:19 +0200

    feat: add card list and card components, add types for cards

---
+++ react-components/src/components/CardsList.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Card } from '../components/Card';
+import { MOVIE_API_KEY } from '../config';
+import { GenreData, MovieData } from '../interfaces';
+
+const ListWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 3rem;
+`;
+
+export class CardsList extends React.Component<
+  Record<string, unknown>,
+  { moviesData: MovieData[]; genresData: GenreData[] }
+> {
+  constructor(props: Record<string, unknown>) {
+    super(props);
+    this.state = { moviesData: [], genresData: [] };
+  }
+  componentDidMount() {
+    this.getMovieData(1).then((data) =>
+      this.setState((prevState) => ({ ...prevState, moviesData: data.results }))
+    );
+    this.getMovieGenres().then((data) =>
+      this.setState((prevState) => ({ ...prevState, genresData: data.genres }))
+    );
+  }
+
+  async getMovieData(page: number) {
+    const moviesRequest = await fetch(
+      `https://api.themoviedb.org/3/movie/popular?page=${page}&api_key=${MOVIE_API_KEY}`
+    );
+    const moviesData = await moviesRequest.json();
+    return moviesData;
+  }
+
+  async getMovieGenres() {
+    const genresRequest = await fetch(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${MOVIE_API_KEY}`
+    );
+    const genresData = await genresRequest.json();
+    return genresData;
+  }
+
+  getGenresFromMovie(movieGenreIds: number[]) {
+    return this.state.genresData.filter((genre) => movieGenreIds.find((id) => id === genre.id));
+  }
+
+  render() {
+    return (
+      <ListWrapper>
+        {this.state.moviesData.map((movie: MovieData) => (
+          <Card key={movie.id} genres={this.getGenresFromMovie(movie.genre_ids)} {...movie} />
+        ))}
+      </ListWrapper>
+    );
+  }
+}
---
+++ react-components/src/interfaces/index.ts
@@ -0,0 +1,30 @@
+export type MovieData = {
+  adult: boolean;
+  backdrop_path: string;
+  genre_ids: Array<number>;
+  id: number;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+export type GenreData = {
+  id: number;
+  name: string;
+};
+
+export type CardProps = {
+  popularity: number;
+  poster_path: string;
+  release_date: string;
+  title: string;
+  vote_average: number;
+  genres: GenreData[];
+};
