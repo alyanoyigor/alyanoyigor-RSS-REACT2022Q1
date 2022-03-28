be4b8ef5a274258628a457da6bc26ef90a4327ac
Igor <igoralyanoy@gmail.com>
2022-03-28 17:37:44 +0300

    feat: add simple test to pages test files

--- react-components/src/pages/About.test.tsx
+++ react-components/src/pages/About.test.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { AppRouter } from '../components/AppRouter';
 import { renderWithRouter } from '../test/helper/renderWithRouter';
 import '@testing-library/jest-dom';
+import { About } from './About';
 
 describe('About', () => {
   it('about location path', () => {
     renderWithRouter(<AppRouter />, '/about-us');
+    expect(screen.getByTestId('about-title')).toBeInTheDocument();
+  });
+  it('Render About page', () => {
+    render(<About />);
     expect(screen.queryByTestId('home-title')).not.toBeInTheDocument();
     expect(screen.getByTestId('about-title')).toBeInTheDocument();
   });
--- react-components/src/pages/Home.test.tsx
+++ react-components/src/pages/Home.test.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { AppRouter } from '../components/AppRouter';
 import { renderWithRouter } from '../test/helper/renderWithRouter';
 import '@testing-library/jest-dom';
+import { Home } from './Home';
 
 describe('Home', () => {
   it('home location path', () => {
     renderWithRouter(<AppRouter />);
     expect(screen.getByTestId('home-title')).toBeInTheDocument();
     expect(screen.getByRole('search')).toBeInTheDocument();
+  });
+  it('Render Home page', () => {
+    render(<Home />);
+    expect(screen.getByTestId('home-title')).toBeInTheDocument();
   });
 });
